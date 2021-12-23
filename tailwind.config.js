e4cf0124c3421aea47ff3fa3a3777820fe7c840e
Francesco <inerba@gmail.com>
2021-12-23 13:14:34 +0100

    more examples

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,31 @@ module.exports = {
 
     media: false, // ex darkMode
 
+    theme: {
+        extend: {
+            colors: {
+                // brand colors ex: 'bg-brand-dark'
+                brand: {
+                    dark: "#162983",
+                    DEFAULT: "#5FB4E5",
+                    light: "#de46d6",
+
+                    secondary: {
+                        DEFAULT: "#a0a0a0",
+                        light: "#e0e0e0",
+                    },
+                },
+            },
+            // class: 'font-press-start'
+            // layout head section ex:
+            // <link rel="preconnect" href="https://fonts.gstatic.com" />
+            // <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet" />
+            fontFamily: {
+                "press-start": ['"Press Start 2P"', "cursive"],
+            },
+        },
+    },
+
     plugins: [
         require("@tailwindcss/typography"), // Use class 'max-w-none' to override the embedded 'max-width' ex: 'prose max-w-none'
         require("@tailwindcss/aspect-ratio"),
